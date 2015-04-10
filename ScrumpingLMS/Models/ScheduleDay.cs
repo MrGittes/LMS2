60396fa35d905f91113796b39ee27a7b7ac1461a
PalleLilja <palle.lilja@gmail.com>
2015-04-10 13:50:57 +0200

    La till att html-attribut ska accepteras för detaljerna

--- ScrumpingLMS/Models/ScheduleDay.cs
+++ ScrumpingLMS/Models/ScheduleDay.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ScrumpingLMS.Models
 {
@@ -18,6 +19,7 @@ namespace ScrumpingLMS.Models
         [ForeignKey("KlassId")]
         public virtual Klass Klass { get; set; }
 
+        [AllowHtmlAttribute]
         public string Details { get; set; }
     }
 }
